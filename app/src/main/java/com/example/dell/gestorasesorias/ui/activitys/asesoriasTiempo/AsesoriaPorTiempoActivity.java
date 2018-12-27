8443dd0f5d71dc42d6ac17a4e9af5f0d4b9dce0b
linkin123 <32915971+linkin123@users.noreply.github.com>
2018-12-27 12:14:14 -0300

    multiple alarmas finish

--- app/src/main/java/com/example/dell/gestorasesorias/ui/activitys/asesoriasTiempo/AsesoriaPorTiempoActivity.java
+++ app/src/main/java/com/example/dell/gestorasesorias/ui/activitys/asesoriasTiempo/AsesoriaPorTiempoActivity.java
@@ -4,9 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.provider.AlarmClock;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -14,6 +12,7 @@ import android.widget.Toast;
 
 import com.example.dell.gestorasesorias.R;
 import com.example.dell.gestorasesorias.base.BaseActivity;
+import com.example.dell.gestorasesorias.ui.MenuActivity;
 
 import java.util.Calendar;
 
@@ -23,6 +22,8 @@ import butterknife.OnClick;
 public class AsesoriaPorTiempoActivity extends BaseActivity {
 
     private static final int ALARM_REQUEST_CODE  = 1;
+    private int mHora;
+    private int mMinutos;
 
     @Override
     protected int getLayoutResId() {
@@ -49,55 +50,54 @@ public class AsesoriaPorTiempoActivity extends BaseActivity {
     @OnClick(R.id.btn_alumn_time_exit)
     public void setHour(){
 
+
         final Calendar c = Calendar.getInstance();
         int hora = c.get(Calendar.HOUR_OF_DAY);
         int minutos = c.get(Calendar.MINUTE);
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,new TimePickerDialog.OnTimeSetListener() {
             @SuppressLint("SetTextI18n")
             @Override
-            public void onTimeSet(TimePicker view, int hora, int minutes) {
-                tvTimeExit.setText(hora+ ":" + minutes);
-
-                int horaActual = c.get(Calendar.HOUR_OF_DAY);
-                int minutosActuales = c.get(Calendar.MINUTE);
-                int horaDuracion = hora-horaActual;
-                int minutosDuracion = minutes -minutosActuales;
-                horaDuracion=horaDuracion*3600;
-                minutosDuracion=minutosDuracion*60;
-                int duracionMiliseg = horaDuracion + minutosDuracion;
-
-                Intent intent = new Intent(getApplicationContext(), MyBroadcastReceiver.class);
-                intent.putExtra("nombre" , etAlumnoNameTime.getText().toString());
-                intent.putExtra("Maestro" , etProfesorNameTime.getText().toString());
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                        getApplicationContext(), 234324243, intent, 0);
-
-
-                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), i, intent, 0);
-                i++;
-                //alarmManager.set(AlarmManager.RTC_WAKEUP, duracionMiliseg, pi);
-
-                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
-                        + (duracionMiliseg * 1000), pendingIntent);
-                Toast.makeText(getApplicationContext(), "Alarma puesta en " + duracionMiliseg + " segundos",
-                        Toast.LENGTH_LONG).show();
+            public void onTimeSet(TimePicker view, int hora, int minutos) {
+                tvTimeExit.setText(hora+ ":" + minutos);
+                    mHora = hora;
+                    mMinutos = minutos;
 
             }
         }, hora , minutos, false);
         timePickerDialog.show();
     }
 
-    private void setAlarm(int hora, int minutes) {
-        Intent i = new Intent(AlarmClock.ACTION_SET_ALARM)
-                .putExtra(AlarmClock.EXTRA_MESSAGE, "alumno :" + etAlumnoNameTime.getText().toString()
-                        + "Maestro : "+ etProfesorNameTime.getText().toString())
-                .putExtra(AlarmClock.EXTRA_HOUR, hora)
-                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+    @OnClick(R.id.btn_alumn_time_start)
+    public void initClass(){
+        final Calendar c = Calendar.getInstance();
+        int horaActual = c.get(Calendar.HOUR_OF_DAY);
+        int minutosActuales = c.get(Calendar.MINUTE);
+        int horaDuracion = mHora-horaActual;
+        int minutosDuracion = mMinutos -minutosActuales;
+        horaDuracion=horaDuracion*3600;
+        minutosDuracion=minutosDuracion*60;
+        int duracionMiliseg = horaDuracion + minutosDuracion;
 
-        if(i.resolveActivity(getPackageManager()) != null){
-            startActivity(i);
-        }
+        Intent intent = new Intent(getApplicationContext(), MyBroadcastReceiver.class);
+        intent.putExtra("nombre" , etAlumnoNameTime.getText().toString());
+        intent.putExtra("Maestro" , etProfesorNameTime.getText().toString());
+        intent.putExtra("id" ,  (int) System.currentTimeMillis());
+        final int _id = (int) System.currentTimeMillis();
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getApplicationContext(), _id, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (duracionMiliseg * 1000), pendingIntent);
+        Toast.makeText(getApplicationContext(), "Alarma puesta en " + duracionMiliseg + " segundos",
+                Toast.LENGTH_LONG).show();
+
+        startActivity(new Intent(this , AsesoriaPorTiempoActivity.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this , MenuActivity.class ) );
     }
 
 
--- app/src/main/java/com/example/dell/gestorasesorias/ui/activitys/asesoriasTiempo/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/dell/gestorasesorias/ui/activitys/asesoriasTiempo/MyBroadcastReceiver.java
@@ -11,38 +11,38 @@ import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.example.dell.gestorasesorias.R;
-import com.example.dell.gestorasesorias.base.BaseActivity;
-import com.example.dell.gestorasesorias.ui.activitys.alumnos.perfilAlumno.ClasesDialog;
+import com.example.dell.gestorasesorias.ui.MenuActivity;
+
+import java.nio.channels.InterruptedByTimeoutException;
 
 public class MyBroadcastReceiver extends android.content.BroadcastReceiver {
 
-
-    int i=0;
-
     @Override
     public void onReceive(android.content.Context context, android.content.Intent intent) {
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, i, intent, 0);
-        i++;
+        Intent notificationIntent = new Intent(context , MenuActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         String nameAlumn = intent.getExtras().getString("nombre");
         String nameMaestro = intent.getExtras().getString("Maestro");
+        int idVenta = intent.getExtras().getInt("id");
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_alumnos)
                 .setContentTitle("Gestor de asesorias")
                 .setContentIntent(pendingIntent)
-                .setContentText("alumno: " + nameAlumn + "Maestro: " + nameMaestro)
+                .setContentText("alumno : " + nameAlumn)
+                .setSubText( "Maestro : " + nameMaestro)
                 .setSound(soundUri); //This sets the sound to play
 
         //Display notification
-        notificationManager.notify(0, mBuilder.build());
+        notificationManager.notify(idVenta, mBuilder.build());
 
-        Toast.makeText(context, "Don't panik but your time is up!!!!.",
+        Toast.makeText(context, "Alumno sale de clases !.",
                 Toast.LENGTH_LONG).show();
         // Vibrate the mobile phone
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(2000);
 
     }
-
 }
